4b0c25b01ee65e5e65cc7386e197563a0b0c29fd
Alexandre <alex.coussediere@yahoo.com>
2026-09-10 08:11:07 +0200

    Add temperatures.ts for temperature analysis
    Implement temperature analysis to find the closest temperature to zero.

---
+++ TypeScript/Classic Puzzles/Easy/temperatures.ts
@@ -0,0 +1,17 @@
+const n: number = parseInt(readline()); // the number of temperatures to analyse
+var inputs: string[] = readline().split(' ');
+let a = null;
+for (let i = 0; i < n; i++) {
+    const t: number = parseInt(inputs[i]);
+    if (a == null){
+        a = t
+    }
+    else if (Math.abs(t) < Math.abs(a) || Math.abs(t) == Math.abs(a) && t > a){
+        a = t
+    }
+}
+if (a == null){
+    a = 0
+}
+
+console.log(a)
