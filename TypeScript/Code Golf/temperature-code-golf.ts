492b0390f9de5b3df53368de7c6bacb02a9f2703
Alexandre <alex.coussediere@yahoo.com>
2026-09-10 08:12:55 +0200

    Add temperature code golf solution

---
+++ TypeScript/Code Golf/temperature-code-golf.ts
@@ -0,0 +1,1 @@
+const n:number=parseInt(readline());var inputs:string[]=readline().split(' ');let a=null;for(let i=0;i<n;i++){const t: number = parseInt(inputs[i]);if(a==null){a=t}else if(Math.abs(t)<Math.abs(a)||Math.abs(t)==Math.abs(a)&&t>a){a=t}}if(a==null){a=0}console.log(a)
